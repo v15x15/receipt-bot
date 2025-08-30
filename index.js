990361820ed69825fab9293f7461c1268e17bd38
v15x15 <v15x15@outlook.com>
2025-08-30 14:12:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ const showUsersBalance = require('./usersbalance');
 
 dotenv.config();
 
+const { Client, GatewayIntentBits } = require('discord.js');
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-  ]
+    GatewayIntentBits.MessageContent,
+  ],
 });
+
 
 const tokenDB = {};
 
