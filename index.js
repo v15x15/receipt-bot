5ec612b8526c0c6d2246e1e18cce8d26af2a66ef
v15x15 <v15x15@outlook.com>
2025-08-30 13:59:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers,
   ]
 });
 
